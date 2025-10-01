51a493d94fd4f93bf99d4bb66c8302dff60d9fe3
utkrishtsharma <utkrishtsharma93@gmail.com>
2025-10-02 04:55:36 +0530

    apps-config.js

---
+++ apps-config.js
@@ -0,0 +1,42 @@
+const APPS_CONFIG = [
+    {
+        name: "Research Portal",
+        icon: "science",
+        url: "research.html"
+    },
+    {
+        name: "Professional Experience",
+        icon: "work",
+        url: "Professional.html"
+    },
+    {
+        name: "Project Portfolio",
+        icon: "folder_special",
+        url: "portfolio.html"
+    },
+    {
+        name: "GitHub",
+        icon: "code",
+        url: "https://github.com/yourusername"
+    },
+    {
+        name: "LinkedIn",
+        icon: "business",
+        url: "https://www.linkedin.com/in/utkrisht-sharma-9b54b0155/"
+    },
+    {
+        name: "Email",
+        icon: "email",
+        url: "mailto:utkrishtsharma93@gmail.com"
+    },
+    {
+        name: "Resume",
+        icon: "description",
+        url: "resume.docx"
+    },
+    {
+        name: "Calendar",
+        icon: "event",
+        url: "https://calendly.com/utkrishtsharma93"
+    }
+];
